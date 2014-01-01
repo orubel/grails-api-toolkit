913bcfad68f20f69ff34738fd52779fa3771e8c3
Owen Rubel <orubel@gmail.com>
2014-01-01 07:53:56 -0800

    updating method to be more complete

--- src/java/net/nosegrind/apitoolkit/Method.java
+++ src/java/net/nosegrind/apitoolkit/Method.java
@@ -1,11 +1,24 @@
 package net.nosegrind.apitoolkit;
 
+/*
+ * Method	Idempotent	Safe
+ * OPTIONS	yes			yes
+ * GET		yes			yes
+ * HEAD		yes			yes
+ * PUT		yes			no
+ * POST		no			no
+ * DELETE	yes			no
+ * PATCH	no			no - See more at: http://restcookbook.com/HTTP%20Methods/idempotency/#sthash.90mexIwP.dpuf
+ */
 
 public enum Method {
+	OPTIONS("OPTIONS"),
 	GET("GET"),
+	HEAD("HEAD"),
 	POST("POST"),
 	PUT("PUT"),
-	DELETE("DELETE");
+	DELETE("DELETE"),
+	PATCH("PATCH");
 
     private String value;
 
