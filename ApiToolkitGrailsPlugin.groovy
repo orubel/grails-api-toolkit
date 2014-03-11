f2e5d0e79275ce9f3de69c6913f9559997f07dda
Owen Rubel <orubel@gmail.com>
2014-03-11 15:08:25 -0700

    updating plugin version

--- ApiToolkitGrailsPlugin.groovy
+++ ApiToolkitGrailsPlugin.groovy
@@ -1,12 +1,12 @@
 import net.nosegrind.apitoolkit.ApiHandlerArtefactHandler
 
 class ApiToolkitGrailsPlugin {
-    def version = "1.0.18"
+    def version = "1.0.19"
     def grailsVersion = "2.3 > *"
     def title = "Api Toolkit"
     def author = "Owen Rubel"
     def authorEmail = "orubel@gmail.com"
-    def description = 'The Grails API Toolkit is a set of tools that automate alot of the tasks needed to build your API by combining the functionality of REST, RPC and HATEOAS. Some included functionality includes api docs, real time notifications / webhooks, generated headers, responsive content type and more.'
+    def description = 'The Grails API Toolkit simplifies api development within Grails while adding in much needed functionality such as apidocs, real time notifications/webhooks, role checking and more. The Api Toolkit is meant to provide all the tools you need in a simplified manner to get up and running with your api without spending days trying to configure and setup.'
     def documentation = "https://github.com/orubel/grails-api-toolkit"
 
     def license = "Apache"
