3c8c817ed8114ffb98051b61f4bf5eab8b2bf9f5
Owen Rubel <orubel@gmail.com>
2014-03-13 09:12:45 -0700

    removing printlns in script for hool controller

--- scripts/ApitoolkitInit.groovy
+++ scripts/ApitoolkitInit.groovy
@@ -70,8 +70,6 @@ private void copyControllersAndViews() {
 	copyFile "$templateDir/hook/show.gsp.template", "$appDir/views/hook/show.gsp"
 
 	String dir2 = packageToDir(packageName)
-	println("${templateDir}/hook/HookController.groovy.template")
-	println("${appDir}/controllers/${dir2}HookController.groovy")
 	generateFile "$templateDir/hook/HookController.groovy.template", "$appDir/controllers/${dir2}HookController.groovy"
 	printMessage "Controller / Views created..."
 }
